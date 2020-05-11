43c58f51203d5cdf719a3c3d4b0299f65de491f1
Jason Houle <jasonmhoule@gmail.com>
2020-05-11 15:00:48 -0500

    Create manitest.py

---
+++ config/manitest.py
@@ -0,0 +1,28 @@
+def manmaker(context):
+  
+  MANIFEST = """
+  apiVersion: v1
+  kind: Pod
+  metadata:
+    name: rstudio-host
+  spec:
+    containers:
+    - name: rstudio-host
+      image: gcr.io/ml-learning-199501/github.com/jasonmhoule/ex1:latest
+      imagePullPolicy: Always
+      env:
+      - name: PASSWORD
+        value: {password}
+      - name: USER
+        value: {user}
+      - name: ROOT
+        value: true
+      stdin: true
+      tty: true
+      ports:
+      - containerPort: 8787
+        hostPort: 8787
+      restartPolicy: Always
+  """.format(**context.properties)
+  
+  return MANIFEST
